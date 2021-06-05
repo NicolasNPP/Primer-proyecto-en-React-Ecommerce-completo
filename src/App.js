c279327069cf36ba2783cdd1bcbe62341456c7d3
NicolasPP <nicolasnahuel94@gmail.com>
2021-06-05 18:40:24 -0300

    1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Nico ecommerce.
         </p>
         <a
           className="App-link"
