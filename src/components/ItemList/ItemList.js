97d724e3d6773f5159cf0002f231caedba310a6b
NicolasPP <nicolasnahuel94@gmail.com>
2021-08-01 17:39:17 -0300

    Arreglo estetico

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,23 +1,27 @@
 import React, { Suspense, lazy } from 'react';
 import Loading from '../Loading/Loading';
-import { Container, Row, Col } from 'reactstrap';
 const ItemSemantic = lazy(() => import('../ItemSemantic/ItemSemantic'))
 
 
 const ItemList = ({ data }) => {
 
     return (
-        <div>
+        <div className="container">
+            <div className="row">
+                {data.map(function (num) {
+                    return <div key={num.id} className="col-md-3">
 
-            <Container>
-                <Row>
-                    {data.map(function (num) {
-                        return <Col md="3"><div key={num.id}><Suspense fallback={<Loading></Loading>}><ItemSemantic name={num.name} price={num.price} description={num.description} id={num.id} img={num.photo} stock={num.stock} /></Suspense></div></Col>
+                        <Suspense fallback={<Loading></Loading>}><ItemSemantic name={num.name} price={num.price} description={num.description} id={num.id} img={num.photo} stock={num.stock} /></Suspense>
 
-                    })}
-                </Row>
-            </Container>
-        </div>
+                    </div>
+
+                })
+
+                }
+
+
+            </div>
+        </div >
     )
 }
 
